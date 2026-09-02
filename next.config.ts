5dc76f814867e5891762555e2fc39e3ce4feff82
Ahmad Mujtabba <ahmadmujtabba29@gmail.com>
2026-09-02 18:49:15 +0500

    Initialized the next app

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
+import dotenv from "dotenv";
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+dotenv.config();
+
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
